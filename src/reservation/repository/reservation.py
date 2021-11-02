a4151a1a8d708a36fa39d56224d6c9cde81db25f
PitchapaSaelim <pitchapa.sae@ku.th>
2021-11-02 22:14:44 +0700

    Can get all new reservations

--- src/reservation/repository/reservation.py
+++ src/reservation/repository/reservation.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import Session
 from .. import models, schemas
-from datetime import datetime
+from datetime import datetime, date
 from fastapi import status, HTTPException
+from sqlalchemy import func
+
 
 def get_all(db: Session):
     reservations = db.query(models.Reservation).all()
     return reservations
+
 
 def create(request: schemas.Reservation, db: Session, current_user: schemas.User):
     request_body = {
@@ -18,31 +21,43 @@ def create(request: schemas.Reservation, db: Session, current_user: schemas.User
     db.refresh(new_reservation)
     return new_reservation
 
+
 def get(reservation_id: int, db: Session):
-    reservation = db.query(models.Reservation).filter(models.Reservation.reservation_id == reservation_id)
+    reservation = db.query(models.Reservation).filter(
+        models.Reservation.reservation_id == reservation_id)
     if not reservation.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No reservation with this id")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No reservation with this id")
     else:
         return reservation.first()
 
+
 def delete(reservation_id: int, db: Session):
-    reservation = db.query(models.Reservation).filter(models.Reservation.reservation_id == reservation_id)
+    reservation = db.query(models.Reservation).filter(
+        models.Reservation.reservation_id == reservation_id)
     if not reservation.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No reservation with this id")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No reservation with this id")
     reservation.delete()
     db.commit()
     return "Delete Successfully"
 
+
 def get_from_specific_date(selected_year: int, selected_month: int, selected_day: int, db: Session):
     min_datetime = datetime(selected_year, selected_month, selected_day, 0, 0)
-    max_datetime = datetime(selected_year, selected_month, selected_day, 23, 59, 59, 999999)
-    reservation = db.query(models.Reservation).filter(models.Reservation.register_timestamp.between(min_datetime, max_datetime)).all()
+    max_datetime = datetime(selected_year, selected_month,
+                            selected_day, 23, 59, 59, 999999)
+    reservation = db.query(models.Reservation).filter(
+        models.Reservation.register_timestamp.between(min_datetime, max_datetime)).all()
     return reservation
 
+
 def update(reservation_id: int, request: schemas.Reservation, db: Session, current_user: schemas.User):
-    reservation = db.query(models.Reservation).filter(models.Reservation.reservation_id == reservation_id)
+    reservation = db.query(models.Reservation).filter(
+        models.Reservation.reservation_id == reservation_id)
     if not reservation.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No reservation with this id")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No reservation with this id")
     request_body = {
         "register_timestamp": request.register_timestamp,
         "user_id": current_user.user_id
@@ -50,3 +65,9 @@ def update(reservation_id: int, request: schemas.Reservation, db: Session, curre
     reservation.update(request_body)
     db.commit()
     return reservation.first()
+
+
+def get_all_new(db: Session):
+    all_new_reservations = db.query(models.Reservation).filter(
+        func.DATE(models.Reservation.register_timestamp) == date.today()).all()
+    return all_new_reservations
--- src/reservation/routers/reservation.py
+++ src/reservation/routers/reservation.py
@@ -12,21 +12,24 @@ router = APIRouter(
     tags=['Reservations']
 )
 
+
 @router.get('/', response_model=List[ShowReservation])
-def get_all_reservation(db: Session=Depends(get_db)):
+def get_all_reservations(db: Session = Depends(get_db)):
     """
-    Get all the reservations that is created
+    Get all the reservations that are created
     """
     return reservation.get_all(db)
 
-@router.post('/', 
-    response_model=ShowReservation, 
-    status_code=status.HTTP_201_CREATED, 
-    summary="Create a Reservation", 
-    responses={status.HTTP_201_CREATED: {"model": ShowReservation, "description": "Create Successful"},
-        status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"}
-    })
-def create_reservation(request: Reservation, db: Session=Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+
+@router.post('/',
+             response_model=ShowReservation,
+             status_code=status.HTTP_201_CREATED,
+             summary="Create a Reservation",
+             responses={status.HTTP_201_CREATED: {"model": ShowReservation, "description": "Create Successful"},
+                        status.HTTP_401_UNAUTHORIZED: {
+                            "model": UnauthorizedResponse, "description": "Send a request but not authenticated"}
+                        })
+def create_reservation(request: Reservation, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     """
     Create a reservation with this information (need to authorize):
     ### Request Body:
@@ -34,11 +37,12 @@ def create_reservation(request: Reservation, db: Session=Depends(get_db), curren
     """
     return reservation.create(request, db, current_user)
 
-@router.get('/{reservation_id}', 
-    response_model=ShowReservation,
-    responses={status.HTTP_404_NOT_FOUND: {"model": NotFoundResponse, "description": "Send a request but no object was found"}
-    })
-def get_reservation(reservation_id: int, db: Session=Depends(get_db)):
+
+@router.get('/{reservation_id}',
+            response_model=ShowReservation,
+            responses={status.HTTP_404_NOT_FOUND: {"model": NotFoundResponse, "description": "Send a request but no object was found"}
+                       })
+def get_reservation(reservation_id: int, db: Session = Depends(get_db)):
     """
     Get the specific reservation's detail from reservation's id:
     ### Parameters:
@@ -46,8 +50,9 @@ def get_reservation(reservation_id: int, db: Session=Depends(get_db)):
     """
     return reservation.get(reservation_id, db)
 
+
 @router.get('/{year}/{month}/{day}', response_model=List[ShowReservation],)
-def get_reservations_from_specific_date(year: int, month: int, day: int, db: Session=Depends(get_db)):
+def get_reservations_from_specific_date(year: int, month: int, day: int, db: Session = Depends(get_db)):
     """
     Get the specific reservation's detail from the specific year, month, and day:
     ### Parameters:
@@ -57,11 +62,13 @@ def get_reservations_from_specific_date(year: int, month: int, day: int, db: Ses
     """
     return reservation.get_from_specific_date(year, month, day, db)
 
+
 @router.delete('/{reservation_id}',
-    responses={ status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
-        status.HTTP_404_NOT_FOUND: {"model": NotFoundResponse, "description": "Send a request but no object was found"}
-    })
-def delete_reservation(reservation_id: int, db: Session=Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+               responses={status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
+                          status.HTTP_404_NOT_FOUND: {
+                              "model": NotFoundResponse, "description": "Send a request but no object was found"}
+                          })
+def delete_reservation(reservation_id: int, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     """
     Delete the specific reservation from reservation's id:
     ### Parameters:
@@ -69,11 +76,13 @@ def delete_reservation(reservation_id: int, db: Session=Depends(get_db), current
     """
     return reservation.delete(reservation_id, db)
 
+
 @router.put('/{reservation_id}', response_model=ShowReservation,
-    responses={status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
-        status.HTTP_404_NOT_FOUND: {"model": NotFoundResponse, "description": "Send a request but no object was found"}
-    })
-def update_reservation(reservation_id: int, request: Reservation, db: Session=Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+            responses={status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
+                       status.HTTP_404_NOT_FOUND: {
+                           "model": NotFoundResponse, "description": "Send a request but no object was found"}
+                       })
+def update_reservation(reservation_id: int, request: Reservation, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
     """
     Update the specific reservation from reservation's id with this information (need to authorize):
     ### Parameters:
@@ -82,3 +91,11 @@ def update_reservation(reservation_id: int, request: Reservation, db: Session=De
     - **register_timestamp**: the datetime as an ISO format e.g., "2021-10-22T14:52:14.933Z"
     """
     return reservation.update(reservation_id, request, db, current_user)
+
+
+@router.get('/new/', response_model=List[ShowReservation])
+def get_all_new_reservations(db: Session = Depends(get_db)):
+    """
+    Get all reservations that are created today
+    """
+    return reservation.get_all_new(db)
