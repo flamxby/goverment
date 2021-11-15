f3551105b015ab1234ed4ce9ebc8fe4df0423b20
Pakarat Kongpaisan <pakarat@MacBook-Pro-khxng-Pakarat.local>
2021-11-15 19:45:23 +0700

    Add field vaccinated to the reservation model and endpoint for update it

--- src/reservation/routers/reservation.py
+++ src/reservation/routers/reservation.py
@@ -1,19 +1,23 @@
 from fastapi import APIRouter, Depends, status
 from typing import List
 
-from ..schemas import ShowReservation, Reservation, User, UnauthorizedResponse, NotFoundResponse
+from ..schemas import (
+    ShowReservation,
+    Reservation,
+    User,
+    UnauthorizedResponse,
+    NotFoundResponse,
+    NoPermissionResponse,
+)
 from ..database import get_db
 from .. import oauth2
 from sqlalchemy.orm import Session
 from ..repository import reservation
 
-router = APIRouter(
-    prefix='/reservation',
-    tags=['Reservations']
-)
+router = APIRouter(prefix="/reservation", tags=["Reservations"])
 
 
-@router.get('/', response_model=List[ShowReservation])
+@router.get("/", response_model=List[ShowReservation])
 def get_all_reservations(db: Session = Depends(get_db)):
     """
     Get all the reservations that are created
@@ -21,15 +25,27 @@ def get_all_reservations(db: Session = Depends(get_db)):
     return reservation.get_all(db)
 
 
-@router.post('/',
-             response_model=ShowReservation,
-             status_code=status.HTTP_201_CREATED,
-             summary="Create a Reservation",
-             responses={status.HTTP_201_CREATED: {"model": ShowReservation, "description": "Create Successful"},
-                        status.HTTP_401_UNAUTHORIZED: {
-                            "model": UnauthorizedResponse, "description": "Send a request but not authenticated"}
-                        })
-def create_reservation(request: Reservation, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+@router.post(
+    "/",
+    response_model=ShowReservation,
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a Reservation",
+    responses={
+        status.HTTP_201_CREATED: {
+            "model": ShowReservation,
+            "description": "Create Successful",
+        },
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": UnauthorizedResponse,
+            "description": "Send a request but not authenticated",
+        },
+    },
+)
+def create_reservation(
+    request: Reservation,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(oauth2.get_current_user),
+):
     """
     Create a reservation with this information (need to authorize):
     ### Request Body:
@@ -38,10 +54,16 @@ def create_reservation(request: Reservation, db: Session = Depends(get_db), curr
     return reservation.create(request, db, current_user)
 
 
-@router.get('/{reservation_id}',
-            response_model=ShowReservation,
-            responses={status.HTTP_404_NOT_FOUND: {"model": NotFoundResponse, "description": "Send a request but no object was found"}
-                       })
+@router.get(
+    "/{reservation_id}",
+    response_model=ShowReservation,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "model": NotFoundResponse,
+            "description": "Send a request but no object was found",
+        }
+    },
+)
 def get_reservation(reservation_id: int, db: Session = Depends(get_db)):
     """
     Get the specific reservation's detail from reservation's id:
@@ -51,8 +73,13 @@ def get_reservation(reservation_id: int, db: Session = Depends(get_db)):
     return reservation.get(reservation_id, db)
 
 
-@router.get('/{year}/{month}/{day}', response_model=List[ShowReservation],)
-def get_reservations_from_specific_date(year: int, month: int, day: int, db: Session = Depends(get_db)):
+@router.get(
+    "/{year}/{month}/{day}",
+    response_model=List[ShowReservation],
+)
+def get_reservations_from_specific_date(
+    year: int, month: int, day: int, db: Session = Depends(get_db)
+):
     """
     Get the specific reservation's detail from the specific year, month, and day:
     ### Parameters:
@@ -63,26 +90,52 @@ def get_reservations_from_specific_date(year: int, month: int, day: int, db: Ses
     return reservation.get_from_specific_date(year, month, day, db)
 
 
-@router.delete('/{reservation_id}',
-               responses={status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
-                          status.HTTP_404_NOT_FOUND: {
-                              "model": NotFoundResponse, "description": "Send a request but no object was found"}
-                          })
-def delete_reservation(reservation_id: int, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+@router.delete(
+    "/{reservation_id}",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": UnauthorizedResponse,
+            "description": "Send a request but not authenticated",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "model": NotFoundResponse,
+            "description": "Send a request but no object was found",
+        },
+    },
+)
+def delete_reservation(
+    reservation_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(oauth2.get_current_user),
+):
     """
     Delete the specific reservation from reservation's id:
     ### Parameters:
     - **reservation_id**: the id of the specific reservation as an integer e.g., 1
     """
-    return reservation.delete(reservation_id, db)
+    return reservation.delete(reservation_id, db, current_user)
 
 
-@router.put('/{reservation_id}', response_model=ShowReservation,
-            responses={status.HTTP_401_UNAUTHORIZED: {"model": UnauthorizedResponse, "description": "Send a request but not authenticated"},
-                       status.HTTP_404_NOT_FOUND: {
-                           "model": NotFoundResponse, "description": "Send a request but no object was found"}
-                       })
-def update_reservation(reservation_id: int, request: Reservation, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+@router.put(
+    "/{reservation_id}",
+    response_model=ShowReservation,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": UnauthorizedResponse,
+            "description": "Send a request but not authenticated",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "model": NotFoundResponse,
+            "description": "Send a request but no object was found",
+        },
+    },
+)
+def update_reservation(
+    reservation_id: int,
+    request: Reservation,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(oauth2.get_current_user),
+):
     """
     Update the specific reservation from reservation's id with this information (need to authorize):
     ### Parameters:
@@ -93,9 +146,38 @@ def update_reservation(reservation_id: int, request: Reservation, db: Session = 
     return reservation.update(reservation_id, request, db, current_user)
 
 
-@router.get('/new/', response_model=List[ShowReservation])
+@router.get("/new/", response_model=List[ShowReservation])
 def get_all_new_reservations(db: Session = Depends(get_db)):
     """
     Get all reservations that are created today
     """
     return reservation.get_all_new(db)
+
+
+@router.put(
+    "/report-taken/{reservation_id}",
+    response_model=ShowReservation,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": UnauthorizedResponse,
+            "description": "Send a request but not authenticated",
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "model": NoPermissionResponse,
+            "description": "Send a request but no permission",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "model": NotFoundResponse,
+            "description": "Send a request but no object was found",
+        },
+    },
+)
+def update_report_taken(
+    reservation_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(oauth2.get_current_user),
+):
+    """
+    Update the vaccinated field to true in the reservation model.
+    """
+    return reservation.report_taken(reservation_id, db, current_user)
--- src/reservation/schemas.py
+++ src/reservation/schemas.py
@@ -2,20 +2,24 @@ from pydantic import BaseModel, validator
 from datetime import date, datetime
 from typing import List, Optional
 
+
 class ReservationBase(BaseModel):
     register_timestamp: datetime
 
+
 class Reservation(ReservationBase):
-    class Config():
+    class Config:
         orm_mode = True
+
 
 class ReservationForUser(BaseModel):
     reservation_id: int
     register_timestamp: datetime
 
-    class Config():
+    class Config:
         orm_mode = True
-    
+
+
 class User(BaseModel):
     name: str
     surname: str
@@ -25,13 +29,14 @@ class User(BaseModel):
     address: str
     password: str
 
-    @validator('citizen_id')
+    @validator("citizen_id")
     def check_citizen_id(cls, value: str):
         if not value.isdigit():
-            raise ValueError('citizen id must be a digit')
+            raise ValueError("citizen id must be a digit")
         if len(value) != 13:
-            raise ValueError('citizen id must have 13 digits')
+            raise ValueError("citizen id must have 13 digits")
         return value
+
 
 class ShowUser(BaseModel):
     user_id: int
@@ -42,8 +47,9 @@ class ShowUser(BaseModel):
     address: str
     reservations: List[ReservationForUser] = []
 
-    class Config():
+    class Config:
         orm_mode = True
+
 
 class ShowUserInfo(BaseModel):
     name: str
@@ -53,30 +59,41 @@ class ShowUserInfo(BaseModel):
     occupation: str
     address: str
 
-    class Config():
+    class Config:
         orm_mode = True
+
 
 class ShowReservation(BaseModel):
     reservation_id: int
     register_timestamp: datetime
+    vaccinated: bool
     owner: ShowUserInfo
 
-    class Config():
+    class Config:
         orm_mode = True
+
 
 class Login(BaseModel):
     citizen_id: str
     password: str
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     citizen_id: Optional[str] = None
+
 
 class UnauthorizedResponse(BaseModel):
     detail: Optional[str] = "Not authenticated"
 
+
 class NotFoundResponse(BaseModel):
     detail: Optional[str] = "No reservation with this id"
+
+
+class NoPermissionResponse(BaseModel):
+    detail: Optional[str] = "No permission"
