683ddcf2b0e58e74640017da354d6dcb7180b6a1
nuttapol-kor <nuttapol.kor@ku.th>
2021-11-16 15:39:41 +0700

    enable CORS

--- src/reservation/main.py
+++ src/reservation/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import reservation, user, authentication
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
